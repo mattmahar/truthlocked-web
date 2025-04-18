649378142621e0ba1c03c62795f0cf2e29e313ff
mattmahar <maharmatt@yahoo.com>
2025-04-18 12:25:16 -0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,4 @@
+// Home Page (Vault Entry)
+export default function Home() {
+  return <main className="text-white bg-black min-h-screen flex items-center justify-center">Hello TruthLocked</main>;
+}

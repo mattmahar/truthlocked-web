308acb632c14976d794be4208a896dc19c8eedd7
mattmahar <maharmatt@yahoo.com>
2025-04-18 12:30:58 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-3xl font-light">Welcome to TruthLocked</h1>
+    </main>
+  );
+}
